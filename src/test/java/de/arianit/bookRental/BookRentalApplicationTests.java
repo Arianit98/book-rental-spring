92fc15855881afae23df68c57a41de9ee941d4e8
arianitmehana <arianitmehana00@gmail.com>
2025-01-13 22:10:53 +0100

    initial commit

---
+++ src/test/java/de/arianit/bookRental/BookRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package de.arianit.bookRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
