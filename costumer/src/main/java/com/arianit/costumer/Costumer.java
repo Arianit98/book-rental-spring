44c6d86ef50ab6c74439c36c2ceb02bda0b4bdf4
arianitmehana <arianitmehana00@gmail.com>
2025-01-17 23:45:23 +0100

    fully functional costumer service

--- costumer/src/main/java/com/arianit/costumer/Costumer.java
+++ costumer/src/main/java/com/arianit/costumer/Costumer.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Costumer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
     private String name;
     private String email;
--- costumer/src/main/java/com/arianit/costumer/CostumerController.java
+++ costumer/src/main/java/com/arianit/costumer/CostumerController.java
@@ -1,5 +1,7 @@
 package com.arianit.costumer;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,23 +22,30 @@ public class CostumerController {
     }
 
     @GetMapping("/{id}")
-    public Costumer getCostumer(@PathVariable Long id) {
-        return costumerService.getCostumer(id);
+    public ResponseEntity<Costumer> getCostumer(@PathVariable("id") Long id) {
+        Costumer costumer = costumerService.getCostumer(id);
+        if (costumer == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(costumer);
     }
 
     @PostMapping
-    public Costumer createCostumer(@RequestBody Costumer request) {
-        return costumerService.createCostumer(request);
+    public ResponseEntity<Costumer> createCostumer(@RequestBody Costumer newCostumer) {
+        Costumer costumer = costumerService.createCostumer(newCostumer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(costumer);
     }
 
     @PutMapping("/{id}")
-    public Costumer updateCostumer(@PathVariable Long id, @RequestBody Costumer newCostumer) {
-        return costumerService.updateCostumer(id, newCostumer);
+    public ResponseEntity<Costumer> updateCostumer(@PathVariable("id") Long id, @RequestBody Costumer newCostumer) {
+        Costumer costumer = costumerService.updateCostumer(id, newCostumer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(costumer);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCostumer(@PathVariable Long id) {
+    public ResponseEntity<?> deleteCostumer(@PathVariable("id") Long id) {
         costumerService.deleteCostumer(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 
