43b781832c89df6ebdf8289e8d213e2221dea03a
arianitmehana <arianitmehana00@gmail.com>
2025-10-04 23:39:55 +0200

    feat: update Feign client URLs in application.properties and adjust BookClient and CostumerClient interfaces

--- reservation/src/main/java/com/arianit/reservation/client/BookClient.java
+++ reservation/src/main/java/com/arianit/reservation/client/BookClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(name = "${book.client.name}", url = "${book.client.url}")
+@FeignClient(name = "${book.client.name}", url = "${book.client.url}/api/v1/books")
 public interface BookClient {
 
     @GetMapping("/{id}")
--- reservation/src/main/java/com/arianit/reservation/client/CostumerClient.java
+++ reservation/src/main/java/com/arianit/reservation/client/CostumerClient.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${costumer.client.name}", url = "${costumer.client.url}")
+@FeignClient(name = "${costumer.client.name}", url = "${costumer.client.url}/api/v1/costumers")
 public interface CostumerClient {
 
     @GetMapping
